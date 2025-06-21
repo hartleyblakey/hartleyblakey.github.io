59c1fc9ad89e0955cf1f553c680ae2a8c511f1f5
Hartley Blakey <hartleyblakey@gmail.com>
2025-06-21 12:28:15 -0700

    test REST API

---
+++ rest/rest.js
@@ -0,0 +1,71 @@
+var entries;
+
+async function newEntry() {
+    let response = await fetch("https://diorama.asherville.dev/api/dioramas", {
+        method: "POST",
+        body: JSON.stringify({}),
+        headers: {
+            "Content-type": "application/json"
+        }
+    });
+
+    updateEntries();
+}
+
+
+function addCard(uuid, properties) {
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('generated');
+    cardDiv.classList.add('project');
+    const labelElement = document.createElement('p');
+    labelElement.classList.add('label');
+    labelElement.textContent = uuid;
+
+    const editTextElement = document.createElement('textarea');
+    editTextElement.textContent = properties.text;
+
+    const updateButtonElement = document.createElement('button');
+    updateButtonElement.textContent = "Save";
+
+    cardDiv.appendChild(labelElement);
+    cardDiv.appendChild(editTextElement);
+    cardDiv.appendChild(updateButtonElement);
+
+    document.getElementById('mainContainer').prepend(cardDiv);
+
+    /*
+        <textarea> Hello, World!</textarea>
+        <button id="createButton"> Save</button>
+    */
+
+}
+
+async function updateEntries() {
+    let response = await fetch("https://diorama.asherville.dev/api/dioramas", {
+        method: "GET",
+        body: JSON.stringify({}),
+        headers: {
+            "Content-type": "application/json"
+        }
+    });
+
+    entries = JSON.parse(await response.json());
+    
+    document.querySelectorAll('.generated').forEach(element => {
+        element.remove();
+    })
+
+    for (const [uuid, properties] of Object.entries(entries)) {
+        addCard(uuid, properties);
+    }
+}
+
+
+
+function onLoad() {
+    var button = document.getElementById('createButton');
+    
+    if (!button) return;
+    button.addEventListener("click", newEntry);
+}
+window.addEventListener("load", onLoad);
